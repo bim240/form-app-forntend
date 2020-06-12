a63a3bfed23fd0dfc7ce8aeb082cdaa4712a170a
bim240 <bimlendu357@gmail.com>
2020-06-12 21:14:12 +0530

    edit quiz

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from "react";
+import "./assets/stylesheets/main.scss";
 import "bulma/css/bulma.css";
+
 import HomePage from "./views/auth/HomePage";
 import { Route, Switch } from "react-router-dom";
-
-import CreateQuiz from "./views/auth/createQuiz";
+import { connect } from "react-redux";
+import CreateQuiz from "./views/quiz/createQuiz";
 import Header from "./views/common/Header";
+import AttemptQuiz from "./views/quiz/attemptQuiz";
 import Login from "./views/auth/login";
 import Signup from "./views/auth/signup";
-import "./assets/stylesheets/main.scss";
+import EditQuiz from "./views/quiz/editQuiz";
 
-function App() {
+import { identifyUser } from "./state/actions/authActions";
+
+function ProtectedRoutes() {
   return (
-    <>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/quiz" component={CreateQuiz} />
-      </Switch>
-    </>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/quiz/new" component={CreateQuiz} />
+      <Route exact path="/quiz/:id" component={AttemptQuiz} />
+      <Route exact path="/quiz/:id/edit" component={EditQuiz} />
+      <Route path="*" render={() => <h1>User already logged in</h1>} />
+    </Switch>
   );
 }
 
-export default App;
+function PublicRoutes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    if (localStorage["auth-token"]) {
+      this.props.dispatch(identifyUser());
+    }
+  }
+
+  render() {
+    const currentUser = this.props.currentUser;
+    return (
+      <>
+        {currentUser.isAuthReqInProgress ? (
+          <div>Loading . . .</div>
+        ) : (
+          <>
+            <Header />
+            {currentUser.userInfo ? <ProtectedRoutes /> : <PublicRoutes />}
+          </>
+        )}
+      </>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { currentUser: state.currentUser };
+}
+export default connect(mapStateToProps)(App);
--- src/views/auth/HomePage.js
+++ src/views/auth/HomePage.js
@@ -1,14 +1,95 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { quizList, deleteQuiz } from "../../state/actions/quizActions";
 
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
+  }
+  componentDidMount() {
+    this.props.dispatch(quizList());
+  }
+  handleDeleteQuiz = (id) => {
+    console.log(id);
+    this.props.dispatch(deleteQuiz(id));
+  };
+  render() {
+    var currentUser = this.props.currentUser;
+    return (
+      <>
+        {this.props.quizzes ? (
+          <div className="container mt-6">
+            {currentUser ? (
+              <div>
+                {" "}
+                <Link to={`/quiz/new`} className="btn btn-success mb-3">
+                  Create Quiz
+                </Link>
+              </div>
+            ) : null}
 
-function HomePage() {
-  return (
-    <div>
-      <h1>Quiz App</h1>
-      <button>signup</button>
-      <button>login</button>
-    </div>
-  );
+            <div className="row ">
+              {this.props.quizzes.map((quiz, i) => {
+                return (
+                  <div className="col-6 col-md-4 mb-6" key={i}>
+                    <div className="card" style={{ width: "18rem" }}>
+                      <img
+                        className="card-img-top"
+                        style={{
+                          height: "160px",
+                          objectFit: "cover",
+                          filter: " opacity(.7)",
+                        }}
+                        src="https://thumbs.dreamstime.com/z/quiz-test-survey-exam-vector-concept-online-laptop-education-illustration-80657742.jpg"
+                        alt="Card image cap"
+                      />
+
+                      <div className="card-body">
+                        <h5 className="card-title">{quiz.title}</h5>
+                        <Link
+                          to={`/quiz/${quiz._id}`}
+                          className="btn btn-primary">
+                          Play
+                        </Link>
+                        {currentUser.id === quiz.authorId ? (
+                          <>
+                            <Link
+                              to={`/quiz/${quiz._id}/edit`}
+                              className="btn btn-warning mx-1">
+                              Edit
+                            </Link>
+                            <button
+                              onClick={() => this.handleDeleteQuiz(quiz._id)}
+                              className="btn btn-danger mx-1">
+                              Delete
+                            </button>
+                          </>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <h1>Loading</h1>
+        )}
+      </>
+    );
+  }
 }
 
-export default HomePage;
+const mapStateToProps = ({ quiz, currentUser }) => {
+  console.log(currentUser.userInfo);
+  return {
+    quizzes: quiz.quizList.quizzes,
+    currentUser: currentUser.userInfo,
+  };
+};
+
+export default connect(mapStateToProps)(HomePage);
