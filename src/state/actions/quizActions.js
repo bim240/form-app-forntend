d957682a206738244b52ba446ba7d9a076ec2420
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-06-14 12:31:26 +0530

    add attempt quiz

--- src/state/actions/quizActions.js
+++ src/state/actions/quizActions.js
@@ -2,139 +2,153 @@ import axios from "axios";
 const url = "/api/v1";
 
 const setTokenToAxios = (token) => {
-  axios.defaults.headers.common["authorization"] =
-    token || localStorage["auth-token"] || "";
+	axios.defaults.headers.common["authorization"] =
+		token || localStorage["auth-token"] || "";
 };
 
 setTokenToAxios();
 
 const createQuiz = (data) => {
-  return async (dispatch) => {
-    try {
-      // dispatch({ type: "FETCH_CURRENT_USER_START" });
+	return async (dispatch) => {
+		try {
+			// dispatch({ type: "FETCH_CURRENT_USER_START" });
 
-      let quiz = await axios.post(`${url}/quiz`, data);
+			let quiz = await axios.post(`${url}/quiz`, data);
 
-      // dispatch({
-      //   // type: "FETCH_CURRENT_USER_SUCCESS",
-      //   payload: user.data.user,
-      // });
-    } catch (error) {}
-  };
+			// dispatch({
+			//   // type: "FETCH_CURRENT_USER_SUCCESS",
+			//   payload: user.data.user,
+			// });
+		} catch (error) {}
+	};
 };
 
 const quizList = () => {
-  return async (dispatch) => {
-    try {
-      let quizzes = await axios.get(`${url}/quiz`);
-      dispatch({
-        type: "FETCH_QUIZ_LIST",
-        payload: quizzes.data,
-      });
-    } catch (error) {}
-  };
+	return async (dispatch) => {
+		try {
+			let quizzes = await axios.get(`${url}/quiz`);
+			dispatch({
+				type: "FETCH_QUIZ_LIST",
+				payload: quizzes.data,
+			});
+		} catch (error) {}
+	};
 };
 
 const showQuiz = (id) => {
-  // console.log("in action");
-  return async (dispatch) => {
-    try {
-      let quiz = await axios.get(`${url}/quiz/${id}`);
-      dispatch({
-        type: "FETCH_QUIZ",
-        payload: quiz.data.quiz,
-      });
-    } catch (error) {}
-  };
+	// console.log("in action");
+	return async (dispatch) => {
+		try {
+			let quiz = await axios.get(`${url}/quiz/${id}`);
+			dispatch({
+				type: "FETCH_QUIZ",
+				payload: quiz.data.quiz,
+			});
+		} catch (error) {}
+	};
 };
 
 const updateQuestion = (id, data) => {
-  return async (dispatch) => {
-    try {
-      // dispatch({ type: "FETCH_CURRENT_USER_START" });
+	return async (dispatch) => {
+		try {
+			// dispatch({ type: "FETCH_CURRENT_USER_START" });
 
-      let quiz = await axios.put(`${url}/questions/${id}`, data);
+			let quiz = await axios.put(`${url}/questions/${id}`, data);
 
-      // dispatch({
-      //   // type: "FETCH_CURRENT_USER_SUCCESS",
-      //   payload: user.data.user,
-      // });
-    } catch (error) {}
-  };
+			// dispatch({
+			//   // type: "FETCH_CURRENT_USER_SUCCESS",
+			//   payload: user.data.user,
+			// });
+		} catch (error) {}
+	};
 };
 const addNewQuestion = (data) => {
-  return async (dispatch) => {
-    try {
-      // dispatch({ type: "FETCH_CURRENT_USER_START" });
+	return async (dispatch) => {
+		try {
+			// dispatch({ type: "FETCH_CURRENT_USER_START" });
 
-      let quiz = await axios.post(`${url}/questions`, data);
+			let quiz = await axios.post(`${url}/questions`, data);
 
-      // console.log(quiz.data.question);
-      dispatch({
-        type: "ADD_NEW_QUIZ_QUESTION",
-        payload: quiz.data.question,
-      });
-    } catch (error) {}
-  };
+			// console.log(quiz.data.question);
+			dispatch({
+				type: "ADD_NEW_QUIZ_QUESTION",
+				payload: quiz.data.question,
+			});
+		} catch (error) {}
+	};
 };
 
 const deleteQuestion = (id) => {
-  return async (dispatch) =>{
-    try {
-       await axios.delete(`${url}/questions/${id}`);  
-       dispatch({
-        type: "DELETE_QUESTION",
-        payload: id,
-      });
-    } catch (error) {
-       
-    }
-  }
-
-}
+	return async (dispatch) => {
+		try {
+			await axios.delete(`${url}/questions/${id}`);
+			dispatch({
+				type: "DELETE_QUESTION",
+				payload: id,
+			});
+		} catch (error) {}
+	};
+};
 
 const deleteQuiz = (id) => {
-  // console.log("in action");
-  return async (dispatch) => {
-    try {
-      await axios.delete(`${url}/quiz/${id}`);
-      dispatch({
-        type: "DELETE_QUIZ",
-        payload: id,
-      });
-    } catch (error) {}
-  };
+	// console.log("in action");
+	return async (dispatch) => {
+		try {
+			await axios.delete(`${url}/quiz/${id}`);
+			dispatch({
+				type: "DELETE_QUIZ",
+				payload: id,
+			});
+		} catch (error) {}
+	};
 };
 
 const changeQuizTitle = (title) => {
-    return {
-      type: "CHANGE_QUIZ_TITLE",
-      payload:  title,
-      }
+	return {
+		type: "CHANGE_QUIZ_TITLE",
+		payload: title,
+	};
 };
 
-const updateQuizTitle = (id , title) =>{
-  return async (dispatch ) => {
-    try {
-     let updatedQuiz = await axios.put(`${url}/quiz/${id}` ,{ quiz : { title } } );
-      let quizTitle =  updatedQuiz.data.updatedQuizTitle.title;
-      dispatch({
-        type: "UPDATE_QUIZ_TITLE",
-        payload: quizTitle
-      });
-    } catch (error) {}
-  };
-}
-
+const updateQuizTitle = (id, title) => {
+	return async (dispatch) => {
+		try {
+			let updatedQuiz = await axios.put(`${url}/quiz/${id}`, {
+				quiz: { title },
+			});
+			let quizTitle = updatedQuiz.data.updatedQuizTitle.title;
+			dispatch({
+				type: "UPDATE_QUIZ_TITLE",
+				payload: quizTitle,
+			});
+		} catch (error) {}
+	};
+};
+const attemptQuiz = (attempt) => {
+	return async (dispatch) => {
+		try {
+			var quizResult = await axios.post(
+				`${url}/quiz/${attempt.quizId}/attempt`,
+				attempt
+			);
+			console.log(quizResult);
+			dispatch({
+				type: "ATTEMPT_QUIZ",
+				payload: quizResult.data,
+			});
+		} catch (error) {}
+	};
+};
 
 export {
-  createQuiz,
-  quizList,
-  showQuiz,
-  deleteQuiz,
-  updateQuestion,
-  addNewQuestion,
-  deleteQuestion,
-  changeQuizTitle,
-  updateQuizTitle
+	createQuiz,
+	quizList,
+	showQuiz,
+	deleteQuiz,
+	updateQuestion,
+	addNewQuestion,
+	deleteQuestion,
+	changeQuizTitle,
+	updateQuizTitle,
+	attemptQuiz,
 };
--- src/state/reducers/quiz.js
+++ src/state/reducers/quiz.js
@@ -1,65 +1,73 @@
 import { quizList } from "../actions/quizActions";
 
 var intialState = {
-  quizList: [],
-  quiz: "",
+	quizList: [],
+	quiz: "",
+	result: "",
 };
 
 function Quiz(state = intialState, action) {
-  switch (action.type) {
-    case "FETCH_QUIZ_LIST":
-      return { ...state, quizList: action.payload };
-    case "FETCH_QUIZ":
-      return { ...state, quiz: action.payload };
-    case "DELETE_QUIZ":
-      return {
-        ...state,
-        quizList: {
-          ...state.quizList,
-          quizzes: state.quizList.quizzes.filter(
-            (quiz) => quiz._id != action.payload
-          ),
-        },
-      };
-    case "DELETE_QUESTION" :
-      return {
-        ...state,
-        quiz : {
-          ...state.quiz,
-          questions : state.quiz.questions.filter((question) => question._id !== action.payload)
-      }
-    }
+	switch (action.type) {
+		case "FETCH_QUIZ_LIST":
+			return { ...state, quizList: action.payload };
+		case "FETCH_QUIZ":
+			return { ...state, quiz: action.payload };
+		case "DELETE_QUIZ":
+			return {
+				...state,
+				quizList: {
+					...state.quizList,
+					quizzes: state.quizList.quizzes.filter(
+						(quiz) => quiz._id != action.payload
+					),
+				},
+			};
+		case "DELETE_QUESTION":
+			return {
+				...state,
+				quiz: {
+					...state.quiz,
+					questions: state.quiz.questions.filter(
+						(question) => question._id !== action.payload
+					),
+				},
+			};
 
-    // just change quiz title  but not update to backend
-     case "CHANGE_QUIZ_TITLE" : 
-    return {
-      ...state ,
-      quiz : {
-        ...state.quiz,
-        title : action.payload
-      }
-    }
-    case "UPDATE_QUIZ_TITLE": 
-    return {
-       ...state,
-       quiz : {
-         ...state.quiz,
-         title : action.payload
-       }
-    }
-      
-    case "ADD_NEW_QUIZ_QUESTION":
-      console.log(action.payload , state.quiz.questions);
-      return {
-        ...state,
-        quiz: {
-          ...state.quiz,
-           questions : [...state.quiz.questions , action.payload]
-        },
-      };
-    default:
-      return state;
-  }
+		// just change quiz title  but not update to backend
+		case "CHANGE_QUIZ_TITLE":
+			return {
+				...state,
+				quiz: {
+					...state.quiz,
+					title: action.payload,
+				},
+			};
+		case "UPDATE_QUIZ_TITLE":
+			return {
+				...state,
+				quiz: {
+					...state.quiz,
+					title: action.payload,
+				},
+			};
+
+		case "ADD_NEW_QUIZ_QUESTION":
+			console.log(action.payload, state.quiz.questions);
+			return {
+				...state,
+				quiz: {
+					...state.quiz,
+					questions: [...state.quiz.questions, action.payload],
+				},
+			};
+		case "ATTEMPT_QUIZ":
+			return {
+				...state,
+				result: action.payload,
+			};
+		default:
+			return state;
+	}
 }
 
 export default Quiz;
